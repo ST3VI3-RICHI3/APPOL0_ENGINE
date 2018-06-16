fa051cb67616a1c57c77ba8921d07975810768f5
SoldierEngieDemo <terrarialover2400@gmail.com>
2018-06-16 15:41:45 -0700

    Made the blank game window appear

--- engine/src/game.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-using System.Data;
-using System.Drawing;
-using System.Windows.Forms;
-using System.Xml;
-using System.Runtime.InteropServices;
-using System.Reflection;
--- engine/src/main.cs
+++ engine/src/main.cs
@@ -1,29 +1,59 @@
 using System;
+using System.Drawing;
+using System.Windows;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
 using System.Xml;
 using System.Runtime.InteropServices;
 using System.Reflection;
 
 namespace ST3_ENGINE
 {
-    class Program
+    public class Tesesest : Form
     {
-        static void Main(string[] args)
+        private TextBox textBox1;
+        public Tesesest()
         {
-            Program GameProgram = new Program();
-        	  Console.Beep();
-        	  Console.WriteLine("APPOL0 Engine Loading...");
-        	  for (var arg = 0; arg < args.Length; arg++)
-        	  {
-              //Add stuff when needed
-        	  }
+            // Initialize the window...
+            InitializeComponent();
+            //
+            // Code goes here...
+            //
+        }
+        private void InitializeComponent()
+        {
+            // Initialize the window...
+            this.SuspendLayout();
+            this.ClientSize = new Size(1280, 720);
+            this.Text = "Apoll0 Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+        [STAThread]
+        public static void Main()
+        {
+            // Beep and start engine...
+            Console.Beep();
+            Console.WriteLine("APPOL0 Engine Loading...");
+            // Load game files...
             Pakgram paker = new Pakgram();
             string[] gamedata = paker.Pak("-L", "testfile");
+            // Dump packed data to console (Debug, Temporary)...
             Console.WriteLine("Tesesest");
             for (int i=0; i<gamedata.Length; i++)
             {
                 Console.WriteLine(gamedata[i]);
             }
+            // ( ͡° ͜ʖ ͡°)
             Console.WriteLine("Tesesesting SUCCeeded!!!!!!");
+            // Open a window...
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Tesesest());
         }
     }
 }
