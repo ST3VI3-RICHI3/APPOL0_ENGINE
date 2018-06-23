2e6791630f727719817cc3698aa1d78acdcd86a9
ST3VI3 RICHI3 <30847371+ST3VI3-RICHI3@users.noreply.github.com>
2018-06-23 14:01:30 +0100

    Started Re-Work of the SDK (windows).
    The app now loads a output console and a windows forms window.

---
+++ Engine SDK/WinSDK/WinSDK/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinSDK
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Console.Title = "Debug output | APOLL0 SDK";
+            Console.WriteLine("System output check");
+            System.Threading.Thread.Sleep(500);
+            Console.WriteLine("Hello World");
+            Console.Beep();
+            Console.WriteLine("Ok!");
+            System.Threading.Thread.Sleep(500);
+            Console.WriteLine(" ");
+            Console.WriteLine("Initalise window");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Console.WriteLine("Done!");
+            Console.WriteLine(" ");
+            Console.WriteLine("Starting process...");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("Done!");
+            Application.Run(new Form1());
+            
+        }
+    }
+}
