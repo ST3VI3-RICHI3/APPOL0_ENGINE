e5a0bbad9e1c8ca74977ed298cf7ee57c48e045e
ST3VI3 RICHI3 <30847371+ST3VI3-RICHI3@users.noreply.github.com>
2018-06-28 09:14:51 +0100

    partial code updates

---
+++ Engine SDK/WinSDK/WinSDK/CustomCommands.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinSDK
+{
+    class CustomCommands
+    {
+        ///<summary>
+        ///<para>Puts the SDK in command mode.</para>
+        ///<para>Usage: WinSDK.CustomCommands.CommandMode(latch[True / false])</para>
+        ///<para>Note latch determines wether command mode will stay enabled after a command has been sent and ran.</para>
+        ///</summary>
+        public static void CommandMode(bool latch) //latch defines weather it stays in command more or returns to automatic.
+        {
+            Console.Write(Environment.CurrentDirectory+"> ");
+            string command = Console.ReadLine();
+
+        }
+    }
+}
