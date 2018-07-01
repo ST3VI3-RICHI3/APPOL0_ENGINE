e1ec12cdd2d88411bca297ff6253c59d451b592c
ST3VI3 RICHI3 <30847371+ST3VI3-RICHI3@users.noreply.github.com>
2018-07-01 11:57:12 +0100

    added a slogan for the game
    Progress (in order):
    system & software tests
    title bar
    slogan

--- Example game(s)/Cypher Source/Cypher Source/Program.cs
+++ Example game(s)/Cypher Source/Cypher Source/Program.cs
@@ -29,13 +29,14 @@ namespace Cypher_Source
             }
             Console.Clear();
             utility.title("Cipher Source");
-            Text.print(true, "8\"\"\"\"8                               8\"\"\"\"8                              ");
-            Text.print(true, "8    \" e  eeeee e   e eeee eeeee     8      eeeee e   e eeeee  eeee eeee ");
-            Text.print(true, "8e     8  8   8 8   8 8    8   8     8eeeee 8  88 8   8 8   8  8  8 8    ");
-            Text.print(true, "88     8e 8eee8 8eee8 8eee 8eee8e        88 8   8 8e  8 8eee8e 8e   8eee ");
-            Text.print(true, "88   e 88 88    88  8 88   88   8    e   88 8   8 88  8 88   8 88   88   ");
-            Text.print(true, "88eee8 88 88    88  8 88ee 88   8    8eee88 8eee8 88ee8 88   8 88e8 88ee ");
+            Text.print(true, "8\"\"\"\"8                               8\"\"\"\"8                        ");
+            Text.print(true, "8    \" e  eeeee e   e eeee eeeee     8      eeeee e   e eeeee  eeee eeee  ");
+            Text.print(true, "8e     8  8   8 8   8 8    8   8     8eeeee 8  88 8   8 8   8  8  8 8      ");
+            Text.print(true, "88     8e 8eee8 8eee8 8eee 8eee8e        88 8   8 8e  8 8eee8e 8e   8eee   ");
+            Text.print(true, "88   e 88 88    88  8 88   88   8    e   88 8   8 88  8 88   8 88   88     ");
+            Text.print(true, "88eee8 88 88    88  8 88ee 88   8    8eee88 8eee8 88ee8 88   8 88e8 88ee   ");
             Text.print(true, " ");
+            Text.print(true, "                      Encryption is only a fallacy.                        ");
             Text.print(true, " ");
             Console.ReadKey();
         }
