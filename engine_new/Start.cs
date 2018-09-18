4ca46ae880849d173f213c7565221daa7434ad9a
ST3VI3 RICHI3 <30847371+ST3VI3-RICHI3@users.noreply.github.com>
2018-09-18 18:20:14 +0100

    Updated startup EXE

--- engine_new/Start.cs
+++ engine_new/Start.cs
@@ -3,16 +3,34 @@ using System;
 namespace AppStarter {
   public class program {
     public static void Main() {
-      Console.WriteLine("Warning: The Rust language must be installed along with cargo (cargo comes with rust), if theese are not installed please install them at http://rust-lang.org");
-      Console.Write("Press any key to continue.");
-      Console.ReadLine();
-      System.Diagnostics.Process process = new System.Diagnostics.Process();
-      System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-      startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-      startInfo.FileName = "cmd.exe";
-      startInfo.Arguments = "/c cargo run";
-      process.StartInfo = startInfo;
-      process.Start();
+      Console.WriteLine("ST3VI3 STUDIOS 2018, All rights reserved");
+      Console.WriteLine(" ");
+      Console.WriteLine("Warning: The Rust language must be installed along with cargo (cargo comes with rust), if theese are not installed please install by pushing 2");
+      Console.WriteLine(" ");
+      Console.WriteLine("Please select an option:");
+      Console.WriteLine("1) Start the game (default)");
+      Console.WriteLine("2) Install rust");
+      Console.WriteLine("");
+      Console.Write("Press [ENTER] to confirm ");
+      string option = "0";
+      option = Console.ReadLine();
+
+      if (option == "" | option == "1") {
+        System.Diagnostics.Process StartProcess = new System.Diagnostics.Process();
+        System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+        startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+        startInfo.FileName = "cmd.exe";
+        startInfo.Arguments = "/c cargo run";
+        StartProcess.StartInfo = startInfo;
+        StartProcess.Start();
+      }
+      if (option == "2"){
+        System.Diagnostics.Process RustInstall = new System.Diagnostics.Process();
+        System.Diagnostics.ProcessStartInfo RuststartInfo = new System.Diagnostics.ProcessStartInfo();
+        RuststartInfo.FileName = "rustup-init.exe";
+        RustInstall.StartInfo = RuststartInfo;
+        RustInstall.Start();
+      }
     }
   }
 }
