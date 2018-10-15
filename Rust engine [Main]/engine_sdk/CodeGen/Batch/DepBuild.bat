ae7a85e477a4558396b1515088b4773c25700502
ST3VI3 RICHI3 <30847371+ST3VI3-RICHI3@users.noreply.github.com>
2018-10-15 21:18:49 +0100

    Script updates

--- Rust engine [Main]/engine_sdk/CodeGen/Batch/DepBuild.bat
+++ Rust engine [Main]/engine_sdk/CodeGen/Batch/DepBuild.bat
@@ -1,20 +1,20 @@
 @echo off
 
 echo Writing details...
-set ProjName = Test
-set authors = Test
+set ProjName=<ProjName.ini
+echo y | del ProjName.ini
+set Auhors=Test
+echo Project name: %ProjName%
+echo Project authors(s): %Authors%
 echo [package] > Cargo.toml
 echo name = "%ProjName%" >> Cargo.toml
 echo version = "0.1.0" >> Cargo.toml
-echo authors = ["%authors%"] >>Cargo.toml
+echo Auhors = ["%Auhors%"] >>Cargo.toml
 echo Done!
 
-echo #Batch hates newlines so this fills in for it. >> cargo.toml
+echo( >> cargo.toml
 
 echo Writing dependencies...
-set DepNum = 0
 echo [dependencies] >> Cargo.toml
-set /p Deps =<deps.tmp
-echo %Deps% >> Cargo.toml
 echo Done!
 pause
