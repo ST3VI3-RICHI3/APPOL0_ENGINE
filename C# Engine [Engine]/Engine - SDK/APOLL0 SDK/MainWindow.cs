eef79a624a2b87d52405324b44ed55dd49b34b5d
ST3VI3 RICHI3 <30847371+ST3VI3-RICHI3@users.noreply.github.com>
2019-04-09 12:47:45 +0100

    Started work on engine reload, getting a SDK setup as a start point.

---
+++ C# Engine [Engine]/Engine - SDK/APOLL0 SDK/MainWindow.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APOLL0_SDK
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MaxButton_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+            else if (this.WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void MinButton_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState != FormWindowState.Minimized)
+            {
+                this.WindowState = FormWindowState.Minimized;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void FileSystemWatcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            DirectoryInfo info = new DirectoryInfo(Environment.CurrentDirectory);
+            if (info.Exists)
+            {
+                FileTree.Nodes[0].Nodes.Clear();
+                TreeNode Rootnode = new TreeNode("Root");
+                Rootnode.Tag = info;
+                GetDirs(info.GetDirectories(), Rootnode);
+                FileTree.Nodes.Add(Rootnode);
+            }
+        }
+
+        private void GetDirs(DirectoryInfo[] subDirs, TreeNode tree)
+        {
+            TreeNode aNode;
+            DirectoryInfo[] subSubDirs;
+            foreach (DirectoryInfo subDir in subDirs)
+            {
+                aNode = new TreeNode(subDir.Name, 0, 0);
+                aNode.Tag = subDir;
+                aNode.ImageKey = "folder";
+                subSubDirs = subDir.GetDirectories();
+                if (subSubDirs.Length != 0)
+                {
+                    GetDirs(subSubDirs, aNode);
+                }
+                tree.Nodes.Add(aNode);
+            }
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            FileSystemWatcher.Path = Environment.CurrentDirectory;
+            DirectoryInfo info = new DirectoryInfo(Environment.CurrentDirectory);
+            if (info.Exists)
+            {
+                FileTree.Nodes.Clear();
+                TreeNode Rootnode = new TreeNode("Root");
+                Rootnode.Tag = info;
+                GetDirs(info.GetDirectories(), Rootnode);
+                FileTree.Nodes.Add(Rootnode);
+            }
+        }
+    }
+}
